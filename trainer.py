1d232356686361e6b17568f25b0b0112e86db0ba
USER <user@AL01515878.local>
2020-02-17 20:17:27 +0900

    modified config, trainer.py

--- trainer.py
+++ trainer.py
@@ -6,25 +6,31 @@ from data_pipeline import train_dataset_fn, test_dataset_fn, get_vocab, idx2bpew
 class Trainer(object):
 
     def __init__(self, hyp_args):
-        train_src_corpus_path = hyp_args["train_src_corpus_path"]
-        train_tgt_corpus_path = hyp_args["train_tgt_corpus_path"]
-        test_src_corpus_path = hyp_args["test_src_corpus_path"]
-        test_tgt_corpus_path = hyp_args["test_tgt_corpus_path"]
-        src_vocab_path = hyp_args["src_vocab_path"]
-        tgt_vocab_path = hyp_args["tgt_vocab_path"]
-        max_len = hyp_args["max_len"]
-        batch_size = hyp_args["batch_size"]
+        self.train_src_corpus_path = hyp_args["train_src_corpus_path"]
+        self.train_tgt_corpus_path = hyp_args["train_tgt_corpus_path"]
+        self.test_src_corpus_path = hyp_args["test_src_corpus_path"]
+        self.test_tgt_corpus_path = hyp_args["test_tgt_corpus_path"]
+        self.src_vocab_path = hyp_args["src_vocab_path"]
+        self.tgt_vocab_path = hyp_args["tgt_vocab_path"]
+        self.max_len = hyp_args["max_len"]
+        self.batch_size = hyp_args["batch_size"]
+        self.model_path = hyp_args["model_path"]
 
-        train_dataset = train_dataset_fn(train_src_corpus_path, train_tgt_corpus_path,
-                         src_vocab_path, tgt_vocab_path, max_len, batch_size)
+        train_dataset = train_dataset_fn(self.train_src_corpus_path,
+                                         self.train_tgt_corpus_path,
+                                         self.src_vocab_path, self.tgt_vocab_path,
+                                         self.max_len, self.batch_size)
 
-        test_dataset = test_dataset_fn(test_src_corpus_path, test_tgt_corpus_path,
-                         src_vocab_path, tgt_vocab_path, max_len, 1)
+        test_dataset = test_dataset_fn(self.test_src_corpus_path,
+                                       self.test_tgt_corpus_path,
+                                       self.src_vocab_path, self.tgt_vocab_path,
+                                       self.max_len, 1)
 
-        iters = tf.data.Iterator.from_structure(train_dataset.output_types, train_dataset.output_shapes)
+        iters = tf.data.Iterator.from_structure(train_dataset.output_types,
+                                                train_dataset.output_shapes)
         src, tgt = iters.get_next()
 
-        self.tgt_vocab_dict = get_vocab(tgt_vocab_path, False)
+        self.tgt_vocab_dict = get_vocab(self.tgt_vocab_path, False)
 
         # create the initialisation operations
         self.train_init_op = iters.make_initializer(train_dataset)
@@ -42,12 +48,20 @@ class Trainer(object):
 
     def train(self, training_steps):
         print("Now training")
+        saver = tf.train.Saver()
+        ckpt = tf.train.get_checkpoint_state("./model" + self.model_path)
+
         with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
             sess.run(tf.global_variables_initializer())
+            if(ckpt and tf.train.checkpoint_exists(ckpt.model_checkpoint_path)):
+                saver.restore(sess, "./model" + self.model_path)
+                print("Model loaded!")
+
             sess.run(self.train_init_op)
             sess.run(tf.tables_initializer())
             n_train_step = 0
             train_loss_, test_loss_ = 0., 0.
+            best_loss = 1e8
 
             for step in range(training_steps):
                 n_train_step += 1
@@ -62,7 +76,7 @@ class Trainer(object):
                     print("Now for test data")
                     sess.run(self.test_init_op)
                     n_test_step = 0
-                    with open("test_output_{}".format(step), "w") as f:
+                    with open("test_output_{}.txt".format(step), "w") as f:
                         try:
                             while True:
                                 n_test_step += 1
@@ -76,6 +90,10 @@ class Trainer(object):
                             pass
 
                     test_loss = test_loss_ / n_test_step
+                    if test_loss < best_loss:
+                        save_path = saver.save(sess, "./model" + self.model_path)
+                        best_loss = test_loss
+
                     sess.run(self.train_init_op)
 
 
