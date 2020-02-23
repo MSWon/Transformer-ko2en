4575d08e94288165d0d23dac56975aa339ac2755
MSWon <jbk4860@skku.edu>
2020-02-23 17:58:32 +0900

    fixed multi-gpu

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ Created on Tue Jul 30 13:57:59 2019
 
 import argparse
 import yaml
-from trainer import Trainer
-from translate import Translate
 
 if __name__ == '__main__':
 
@@ -23,11 +21,13 @@ if __name__ == '__main__':
         print('{} : {}'.format(key, value))
     print('========================')
     if args.mode == "train":
+        from trainer import Trainer
         ## Build model
         model = Trainer(hyp_args)
         ## Train model
         model.train(hyp_args["training_steps"])
     elif args.mode == "infer":
+        from translate import Translate
         ## Build model
         model = Translate(hyp_args)
         ## Infer model
