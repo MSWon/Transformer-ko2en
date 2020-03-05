28d2255c0ea06335a636d2bcc92bcca87e2e3f47
Minsub Won <jbk4860@skku.edu>
2020-03-06 00:10:30 +0900

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,4 +1,4 @@
-FILEID=1jDZKo1upLpIpD0YuacWj_HshzIKdIxLN
+FILEID=1llEZdcALMJB8AFcJNTRsVpyZX3Brla4e
 FILENAME=data.tar.gz
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='$FILEID'' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $FILENAME && rm -rf /tmp/cookies.txt
