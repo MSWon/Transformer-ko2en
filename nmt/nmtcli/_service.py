9cfd38c23190fcde2f32bd344efe789ee71de85d
minsub.won <minsub.won@navercorp.com>
2020-04-02 22:51:08 +0900

    split nmtservice into restapi,website mode

--- nmt/nmtcli/_service.py
+++ nmt/nmtcli/_service.py
@@ -7,4 +7,9 @@ def nmt_service(args):
     """
     uppath = lambda _path, n: os.sep.join(_path.split(os.sep)[:-n])
     path = uppath(__file__, 2)
-    os.system("python {} --port {}".format(os.path.join(path, "nmtservice/app.py") ,args.port))
+    if args.mode == "restapi":
+        os.system("python {} --port {}".format(os.path.join(path, "nmtservice/app_restapi.py"), args.port))
+    elif args.mode == "website":
+        os.system("python {} --port {}".format(os.path.join(path, "nmtservice/app_website.py"), args.port))
+    else:
+        raise NameError
