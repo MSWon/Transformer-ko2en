f2f2bd8ce7d73c4c94cfee9255a8194854da6f60
MSWon <jbk4860@skku.edu>
2021-06-02 23:00:52 +0900

    update requirements and remove sh files

--- nmt/download_data.sh
+++
@@ -1,8 +0,0 @@
-FILEID=1llEZdcALMJB8AFcJNTRsVpyZX3Brla4e
-FILENAME=data.tar.gz
-SCRIPT_PATH=$( cd "$(dirname "$0")" ; pwd )
-
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='$FILEID'' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILEID" -O $SCRIPT_PATH/$FILENAME && rm -rf /tmp/cookies.txt
-
-tar -zxvf $SCRIPT_PATH/$FILENAME -C $SCRIPT_PATH
-rm $SCRIPT_PATH/$FILENAME
