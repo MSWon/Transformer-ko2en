d328b3d0c5470dcbc05ddb8f1554d5f6c34f1516
MSWon <jbk4860@skku.edu>
2021-06-02 23:00:07 +0900

    modify nmtservice api code

--- nmt/nmtservice/app_website.py
+++ nmt/nmtservice/app_website.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, jsonify
+from nmt.nmtservice.service_transformer import ServiceTransformer
 import yaml
 import argparse
 import os
@@ -22,27 +23,28 @@ db_trans ={'고맙습니다': 'Thank you',
            '형': 'Brother', 
            '남동생': 'Brother', '여동생': 'Sister', '누나': 'Sister', '할머니': 'Grandma', '할아버지': 'Grandpa'}
 
+
 app = Flask(__name__)
 
-@app.route('/nmt', methods=['GET'])
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    input_sent = urllib.parse.unquote(request.args.get("text"))
-    src_type = request.args.get("source")
-    tgt_type = request.args.get("target")
-    if input_sent in db_trans:
-        output_sent = db_trans[input_sent]
-    else:
-        output_sent = model.infer(input_sent)
-    return jsonify({"srcLangType":src_type, "tgtLangType":tgt_type, "translatedText":output_sent})
+    if request.method == 'POST':
+        input_sent = request.form["txtSource"]
+        if input_sent in db_trans:
+            output_sent = db_trans[input_sent]
+        else:
+            output_sent = model.service_infer(input_sent)
+        return render_template("index.html", input_sent=input_sent, output_sent=output_sent)
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    from nmt.nmtservice.service_transformer import ServiceTransformer
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", "-p", help="port number", required=True)
     parser.add_argument("--config_path", "-c", required=True, help="config file path")
     args = parser.parse_args()
-    hyp_args = yaml.load(open(args.config_path))
+    hyp_args = yaml.load(args.config_path)
     ## Build model
     model = ServiceTransformer(hyp_args)
     app.run(host='0.0.0.0', port=args.port, debug=True)
