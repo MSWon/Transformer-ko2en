79dac8d714d4cec15628157c06ae3f8ac6bbdafa
minsub.won <minsub.won@navercorp.com>
2021-08-22 23:55:08 +0900

    bump up version to 1.1.0

--- nmt/__init__.py
+++ nmt/__init__.py
@@ -1,3 +1,7 @@
 __version__ = "1.1.0"
 __description__ = "Package for Translating Korean to English"
 __author__ = "Minsub Won"
+
+from nmt import nmtservice
+from nmt import nmttrain
+from nmt import nmtcli
